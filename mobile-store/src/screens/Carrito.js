79cc103d5de554ae1128313fafa4dd6c985050a3
mjuncos <mjuncos@folderit.net>
2024-02-21 13:13:08 -0300

    carrito

--- mobile-store/src/screens/Carrito.js
+++ mobile-store/src/screens/Carrito.js
@@ -1,30 +1,44 @@
-import React, { useEffect } from 'react';
-import { Text, Button, ScrollView } from 'react-native';
-import { useSelector } from 'react-redux';
-import Product from '../components/Product';
+import React, { useEffect, useState } from "react";
+import { Text, Button, ScrollView } from "react-native";
+import { useSelector } from "react-redux";
+import Product from "../components/Product";
 
 const Carrito = ({ navigation }) => {
-
-  const products = useSelector(
-    state => state.productsReducer.productsInCart
-  )
-
+  const products = useSelector((state) => state.productsReducer.productsInCart);
+  const total = products.reduce((acc, current) => acc + current.price, 0);
   return (
-    <ScrollView style={{ backgroundColor: 'white'}}>
-      {products.length>0 ?  products.map((element, index) => (
-          <Product carrito key={index} product={element}/>
+    <ScrollView style={{ backgroundColor: "white" }}>
+      {products.length > 0 ? (
+        products.map((element, index) => (
+          <Product carrito key={index} product={element} />
         ))
-      :
-       <Text style={{ fontSize: 18, color: "blue", alignSelf: 'center', marginVertical: 300 }}>
-        Aún no tienes productos en el carrito</Text>
-      
-      }
-      
-      <Button
-        title="Volver"
-        onPress={() => navigation.navigate('Productos')}
-      />
-      </ScrollView>
+      ) : (
+        <Text
+          style={{
+            fontSize: 18,
+            color: "blue",
+            alignSelf: "center",
+            marginVertical: 300,
+          }}
+        >
+          Aún no tienes productos en el carrito
+        </Text>
+      )}
+
+      {products.length > 0 && (
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: "blue",
+                  alignSelf: "center",
+                  marginVertical: 10,
+                }}
+              >
+                Total: ${total.toFixed(2)}
+              </Text>
+            )}
+      <Button title="Volver" onPress={() => navigation.navigate("Productos")} />
+    </ScrollView>
   );
 };
 
