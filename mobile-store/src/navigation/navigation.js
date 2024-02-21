b66680e01bdc3ee35008ef7fdd76565697cb6625
mjuncos <mjuncos@folderit.net>
2024-02-21 17:05:47 -0300

    splash and rebranding

--- mobile-store/src/navigation/navigation.js
+++ mobile-store/src/navigation/navigation.js
@@ -1,9 +1,8 @@
-
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Productos from '../screens/Productos';
-import Carrito from '../screens/Carrito';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Productos from "../screens/Productos";
+import Carrito from "../screens/Carrito";
 
 const Stack = createStackNavigator();
 
@@ -11,8 +10,38 @@ function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Productos">
-        <Stack.Screen name="Productos" component={Productos} />
-        <Stack.Screen name="Carrito" component={Carrito} />
+        <Stack.Screen
+          name="Productos"
+          component={Productos}
+          options={{
+            title: "Productos",
+            headerStyle: {
+              backgroundColor: "#1b1c20",
+            },
+            headerTintColor: "#FFFFFF",
+            headerTitleStyle: {
+              fontFamily: "Mulish",
+              fontWeight: "400",
+              fontSize: 20,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Carrito"
+          component={Carrito}
+          options={{
+            title: "Carrito",
+            headerStyle: {
+              backgroundColor: "#1b1c20",
+            },
+            headerTintColor: "#FFFFFF",
+            headerTitleStyle: {
+              fontFamily: "Mulish",
+              fontWeight: "400",
+              fontSize: 20,
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- mobile-store/src/screens/Carrito.js
+++ mobile-store/src/screens/Carrito.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const Carrito = ({ navigation }) => {
   return (
-    <View>
-      <Text>Carrito</Text>
-      <Button
-        title="Volver"
-        onPress={() => navigation.navigate('Productos')}
-      />
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "#F5F5F5" }}>
+      <Text style={{ marginVertical: 50, fontSize: 20, color: "black" }}>
+        Aquí mostraremos la lista de productos agregados al carrito
+      </Text>
+      <TouchableOpacity
+        style={{ bottom: 20, position: "absolute" }}
+        onPress={() => navigation.navigate("Productos")}
+      >
+        <LinearGradient
+          colors={["#760097", "#FD0079"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradient}
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Text style={styles.buttonText}>Volver</Text>
+          </View>
+        </LinearGradient>
+      </TouchableOpacity>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  buttonText: {
+    fontStyle: 'normal',
+    fontSize: 20,
+    fontFamily: 'Mulish',
+    textAlign: 'center',
+    color: "white",
+  },
+  gradient: {
+    height: 50,
+    borderRadius: 50,
+    textAlign: 'center',
+    width: 350,
+  },
+});
 export default Carrito;
